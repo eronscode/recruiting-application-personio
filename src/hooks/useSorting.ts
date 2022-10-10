ee8f25bee41a33c1b61a244a944c22784a93afdb
Osemudiamen Matthew Elebesunu <eronscode@gmail.com>
2022-10-10 07:18:30 +0100

    feat: adds re-usable hook for sorting

---
+++ src/hooks/useSorting.ts
@@ -0,0 +1,49 @@
+import { useEffect, useMemo, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { sortRows } from "common/methods";
+
+export function useSorting<T>(
+  dataToPaginate: Array<T>,
+  { onSort }: { onSort?: () => void },
+) {
+  const [sort, setSort] = useState({ order: "asc", orderBy: "id" });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const orderFromURL = searchParams.get("order");
+  const orderByFromURL = searchParams.get("orderBy");
+
+  const handleSort = (accessor: string) => {
+    onSort && onSort();
+    setSort((prevSort) => ({
+      order:
+        prevSort.order === "asc" && prevSort.orderBy === accessor
+          ? "desc"
+          : "asc",
+      orderBy: accessor,
+    }));
+    setSearchParams({
+      order: sort.order === "asc" && sort.orderBy === accessor ? "desc" : "asc",
+      orderBy: accessor,
+    });
+  };
+
+  useEffect(() => {
+    if (orderFromURL && orderByFromURL) {
+      setSort({
+        order: orderFromURL,
+        orderBy: orderByFromURL,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const data = useMemo(
+    () => sortRows(dataToPaginate, sort),
+    [dataToPaginate, sort],
+  );
+
+  return {
+    data,
+    sort,
+    handleSort,
+  };
+}
