e7a0fb86d6e5e1d2baca6dfcce48a61ebe32ebf5
Osemudiamen Matthew Elebesunu <eronscode@gmail.com>
2022-10-10 07:22:10 +0100

    feat: adds UserT to global types

---
+++ src/common/types.ts
@@ -0,0 +1,10 @@
+export type UserT = {
+  id: string;
+  name: string;
+  email: string;
+  birth_date: string;
+  year_of_experience: string;
+  position_applied: string;
+  application_date: string;
+  status: string;
+};
