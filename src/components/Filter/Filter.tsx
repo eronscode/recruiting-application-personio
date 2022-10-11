46b92561dd85ef2400581f532f56676cf805b7f9
Osemudiamen Matthew Elebesunu <eronscode@gmail.com>
2022-10-11 07:14:45 +0100

    feat: implements filter component

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,78 @@
+import { FilterE } from "common/enums";
+import { FilterT, SelectOptionT } from "common/types";
+import CustomMultiSelect from "components/CustomMultiSelect";
+import styles from "./Filter.module.css";
+import Input from "components/Input";
+
+type Props = {
+  filterControls: FilterT[];
+  options?: SelectOptionT[][];
+  selectedFilters: {
+    [key: string]: SelectOptionT[] | string;
+  } | null;
+  resetFilters: (value?: any) => void;
+  updateFilters: (value: any, accessor: string) => void;
+};
+
+function Filter({
+  filterControls,
+  selectedFilters,
+  options,
+  resetFilters,
+  updateFilters,
+}: Props) {
+  return (
+    <section className={styles.filterWrapper}>
+      <h1>Filters: </h1>
+      {filterControls?.map((filter, index) => {
+        return (
+          <div key={filter.accessor}>
+            {renderFilterType(
+              filter,
+              options ? options[index] : [],
+              updateFilters,
+              selectedFilters,
+            )}
+          </div>
+        );
+      })}
+      {Object.keys(selectedFilters || {}).length > 0 && (
+        <button onClick={resetFilters} className={styles.clearBtn}>
+          Clear Filters
+        </button>
+      )}
+    </section>
+  );
+}
+
+function renderFilterType(
+  filter: FilterT,
+  options: Array<SelectOptionT>,
+  handleChange: (value: any, accessor: string) => void,
+  selectedFilters: any,
+) {
+  switch (filter.type) {
+    case FilterE.MultiSelect:
+      return (
+        <CustomMultiSelect
+          options={options || []}
+          onChange={(selected) => handleChange(selected, filter.accessor)}
+          placeholder={filter.label}
+          values={selectedFilters ? selectedFilters[filter.accessor] : []}
+        />
+      );
+    case FilterE.Search:
+      return (
+        <Input
+          onValueChange={(selected) => handleChange(selected, filter.accessor)}
+          placeholder={filter.label}
+          value={selectedFilters ? selectedFilters[filter.accessor] : ""}
+        />
+      );
+
+    default:
+      return <p>Invalid Filter Type</p>;
+  }
+}
+
+export default Filter;
