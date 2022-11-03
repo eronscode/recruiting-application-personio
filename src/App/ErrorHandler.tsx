645b3ada791113fc25422aaeb9366ca545044404
Osemudiamen Matthew Elebesunu <eronscode@gmail.com>
2022-11-03 05:52:28 +0100

    feat:adds Error UI to Error boundary

--- src/App/ErrorHandler.tsx
+++ src/App/ErrorHandler.tsx
@@ -1,3 +1,4 @@
+import ErrorUI from 'components/ErrorUI'
 import * as React from 'react'
 
 type Props = {
@@ -18,6 +19,6 @@ export default class ErrorHandler extends React.Component<Props, State> {
   }
 
   render() {
-    return this.state.error ? <div>Error?</div> : this.props.children
+    return this.state.error ? <ErrorUI message='Error Occured. Kindly refresh this page.' /> : this.props.children
   }
 }
